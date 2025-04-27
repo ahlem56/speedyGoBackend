52f365c8c96830b4f60e02a07ef03361b535ffa4
mehdyuchihi <chihi.mehdy@esprit.com>
2025-04-27 17:15:53 +0100

    controle de saisie event

--- src/main/java/tn/esprit/examen/nomPrenomClasseExamen/entities/Event.java
+++ src/main/java/tn/esprit/examen/nomPrenomClasseExamen/entities/Event.java
@@ -1,6 +1,10 @@
 package tn.esprit.examen.nomPrenomClasseExamen.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +23,14 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer eventId ;
+    @NotNull(message = "La date de l'événement est obligatoire")
+    @FutureOrPresent(message = "La date doit être présente ou future")
     private Date eventDate ;
+    @NotBlank(message = "La description ne peut pas être vide")
+    @Size(max = 500, message = "La description ne peut pas dépasser 500 caractères")
     private String eventDescription;
+    @NotBlank(message = "Le lieu ne peut pas être vide")
+    @Size(max = 100, message = "Le lieu ne peut pas dépasser 100 caractères")
     private String eventLocation;
 
     @ManyToMany
